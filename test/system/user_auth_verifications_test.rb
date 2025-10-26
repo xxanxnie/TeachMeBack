f69c4fbb1bde30ed1cb6781478e134e4195321e9
Kiel Malate <kielmalate@MacBookPro.lan>
2025-10-26 17:23:38 -0400

    Add system test for user signup/login .edu verification

---
+++ test/system/user_auth_verifications_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class UserAuthVerificationsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit user_auth_verifications_url
+  #
+  #   assert_selector "h1", text: "UserAuthVerification"
+  # end
+end
